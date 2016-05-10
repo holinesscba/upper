f87f35aa7b38ef3bc6825a0b61f2a26c36a795b4
Marcelo Mogami <marcelomogami@gmail.com>
2016-05-10 05:00:50 +0000

    Fazer post no wordpress

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,35 @@
 class Post < ActiveRecord::Base
+  after_save :post_wordpress, on: [:create]
 
   mount_uploader :file, FileUploader
+
+  private
+    def post_wordpress
+      wp = Rubypress::Client.new(:host => Rails.application.secrets.rubypress_host, 
+                                 :username => Rails.application.secrets.rubypress_username, 
+                                 :password => Rails.application.secrets.rubypress_password)
+
+      post_content =  "<div>"\
+                      "#{ActionController::Base.helpers.cl_image_tag(self.file.full_public_id, :format => "jpg", :width => 600, :height => 851)}<br />"\
+                      "#{ActionController::Base.helpers.cl_image_tag(self.file.full_public_id, :format => "jpg", :width => 600, :height => 851, :page => 2)}<br />"\
+                      "<div>#{self.title}</div>"\
+                      "</div>"
+
+      wp.newPost( :blog_id => 0, # 0 unless using WP Multi-Site, then use the blog id
+                  :content => {
+                               :post_status  => "draft",
+                               :post_date    => Time.now,
+                               :post_content => post_content,
+                               :post_title   => self.title,
+                               :post_name    => "/#{self.title.parameterize}",
+                               :post_author  => 5, # 1 if there is only the admin user, otherwise the user's id
+                               :terms_names  => {
+                                  :category   => ['Boletim Semanal'],
+                                  :post_tag => [Time.now.year]
+                                                }
+                               }
+                  )  
+
+    end
 
 end
